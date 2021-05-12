193d60863b9d787b675799c11028c3dd18a4fef5
huks_wno1@163.com <huks_wno1@163.com>
2021-05-12 21:02:55 +0800

    clearinfo

--- src/views/map/person-house/components/person-house-infomation-page.vue
+++ src/views/map/person-house/components/person-house-infomation-page.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-show="personHouseInfo.show">
     <dialog-drag
-      id="dialog-2"
+      id="dialog-1"
       class="dialog-3"
       :title="personHouseDataFormTitle"
       pinned="false"
@@ -10,22 +10,19 @@
     >
       <!-- <el-scrollbar :native="false" style="height:100%, height: 100%">-->
       <div class="filter-container">
-        <el-input v-model="Address.filter" placeholder="过滤(社区，网格，楼栋)" style="width: 150px;" class="filter-item" @keyup.enter.native="handleLocalFilter" />
+        <el-input v-model="Address.filter" placeholder="过滤(社区，网格，楼栋)" style="width: 180px;" class="filter-item" />
         <el-select v-model="Address.community" placeholder="社区" clearable style="width: 100px" class="filter-item" @change="getNetGridData">
           <el-option v-for="item in filteredCommunities" :key="item.id" :label="item.name" :value="item.id" />
         </el-select>
         <el-select v-model="Address.netGrid" placeholder="网格" clearable style="width: 100px" class="filter-item" @change="getBuildingsData">
         <el-option v-for="item in filteredNetGrids" :key="item.id" :label="'网格' + item.name" :value="item.id" />
       </el-select>
-        <el-select v-model="Address.building" placeholder="楼栋" clearable class="filter-item" style="width: 130px" @change="getRoomsData">
+        <el-select v-model="Address.building" placeholder="楼栋" clearable class="filter-item" style="width: 150px" @change="getRoomsData">
           <el-option v-for="item in filteredBuildings" :key="item.id" :label="item.address + '-' + item.name + '栋'" :value="item.id" />
         </el-select>
-        <el-select v-model="Address.sort" style="width: 140px" class="filter-item" @change="flyToRoom">
+        <el-select v-model="Address.room" style="width: 140px"  class="filter-item" @change="flyToRoom">
           <el-option v-for="item in filteredRooms" :key="item.id" :label="item.name" :value="item.id" />
-        </el-select>
-        <el-button class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-          查询
-        </el-button>
+        </el-select>      
       </div>
       <el-table :data="personHouseInfo.personInRoom" height="350" border style="width: 100%">
         <el-table-column prop="name" label="姓名" width="80" />
@@ -68,16 +65,14 @@ export default {
       option: { top: 250, left: 20, height: 450, width: 800, buttonPin: false },
       communities:[],
       netGrids:[],
-      subdivsions: [],
       buildings: [],
       cureentBuildingName: '', // 当前选择building’s name
       rooms: [],
       Address: {
         community:'',
         netGrid:'',
-        subdivsion: undefined,
-        building: undefined,
-        room: undefined,
+        building: '',
+        room: '',
         filter: ''
 			}
     }
@@ -133,7 +128,6 @@ export default {
   },
   watch: {},
   created() {
-    //this.getSubdivsionsData()
      this.getCommunitysData()
   },
   mounted(){
@@ -146,9 +140,7 @@ export default {
   },
   methods: {
       getCommunitysData() {
-      //debugger
       getCommunitys().then(response => {
-        // debugger
         this.communities = response
       }).catch(error => {
         console.log(error)
@@ -157,7 +149,7 @@ export default {
      getNetGridData(item) {
       // debugger
       if (!item) { // 如果取消社区选取
-        this.netGrids = []
+        this.clearInfo();
         return
       }
       getNetGridInCommunity(item).then(response => {
@@ -170,7 +162,8 @@ export default {
     getBuildingsData(item) {
       // debugger
       if (!item) { // 如果取消网格选取
-        this.buildings = []
+        //this.buildings = []
+        this.clearInfonetGrid();
         return
       }
       getBuildingInNetGrid(item).then(response => {
@@ -180,30 +173,13 @@ export default {
         console.log(error)
       })
     },
-    // getSubdivsionsData() {
-    //   getSubdivsions().then(response => {
-    //     // debugger
-    //     this.subdivsions = response
-    //   }).catch(error => {
-    //     // debugger
-    //     console.log(error)
-    //   })
-    // },
-    // getBuildingsData(item) {
-    //   // debugger
-    //   getBuildingsBySub(item).then(response => {
-    //     // debugger
-    //     this.buildings = response
-    //   }).catch(error => {
-    //     debugger
-    //     console.log(error)
-    //   })
-    // },
     getRoomsData(id) {
-      // debugger
-      const building = this.buildings.find(r => r.id === id)
-      this.cureentBuildingName = building.name // 保持当前选择building’s name
-
+      debugger
+     // const building = this.buildings.find(r => r.id === id)
+     // this.cureentBuildingName = building.name // 保持当前选择building’s name
+      if(!id){
+        this.clearInfobuilding();
+      }
       getRoomByBuilding(id).then(response => {
         // debugger
         this.rooms = response
@@ -217,25 +193,35 @@ export default {
       debugger
 			//定位飞行过程中，信息面板设为不可见
 			this.personHouseInfo.show = false;
-     debugger
       const room = this.rooms.find(r => r.id === roomId)
       if (room) {
-        debugger
         const position = { // 70-2-1002
           long: room.longitude,
           lat: room.latitude,
           height: room.height
         }
-        debugger
         this.personHouseInfo.interactOperate.FlytoRoom(position, room.name)
       }
     },
+      clearInfonetGrid (){
+       this.Address.netGrid = '';
+       this.Address.building = '';
+       this.Address.room = '';
+    },
+     clearInfobuilding (){
+       this.Address.building = '';
+       this.Address.room = '';
+    },
+    clearInfo (){
+       this.Address.community = '';
+       this.Address.netGrid = '';
+       this.Address.building = '';
+       this.Address.room = '';
+    },
     close() {
-      this.personHouseInfo.show = false
+      this.personHouseInfo.show = false;
+      this.clearInfo();
 		},
-		handleFilter(){
-			
-		}
   }
 }
 </script>
