70d8fe05f7e1991aa2cc83dc5dbbe08ca3a0a408
huks_wno1@163.com <huks_wno1@163.com>
2021-11-21 16:52:19 +0800

    00

--- src/views/buildingEconomy/buildingEcoMap/companyInfoComponents/companyInfoPanel.vue
+++ src/views/buildingEconomy/buildingEcoMap/companyInfoComponents/companyInfoPanel.vue
@@ -1,58 +1,62 @@
 <template>
 	<div v-show="companyDataForms.show">
 		<dialog-drag
-      id="dialog-1"
-      class="dialog-3"
-      :title="companyInFloorTitle"
-      pinned="false"
-      :options="option"
-      @close="close"
-    >
-			<filter-panel ref="filterPanel" @firstSelectChange="getFloorInfos" @secondSelectChange="flyToTarget"></filter-panel>
+			id="dialog-1"
+			class="dialog-3"
+			:title="companyInFloorTitle"
+			pinned="false"
+			:options="option"
+			@close="close"
+		>
+			<el-tabs type="border-card">
+				<el-tab-pane label="企业信息">			
+					<filter-panel ref="filterPanel" @firstSelectChange="getFloorInfos" @secondSelectChange="flyToTarget"></filter-panel>
 
-      <el-table :data="companyDataForms.companiesFullInfo" height="250" border style="width: 100%">
-				<el-table-column prop="buildingName" label="楼宇名称" width="120">
-				</el-table-column>
-				<el-table-column prop="companyName" label="企业名称" width="180">
-				</el-table-column>
-				<el-table-column prop="unifiedSocialCreditCode" label="统一社会信用代码" width="180">
-				</el-table-column>
-				<el-table-column prop="actualOfficeAddress" label="实际办公地址" width="180">
-				</el-table-column>
-				<el-table-column prop="registeredCapital" label="注册资本(万元)" width="120">
-				</el-table-column>
-				<el-table-column prop="legalRepresentative" label="公司法人代表" width="140">
-				</el-table-column>
-				<el-table-column prop="contacts" label="联系人" width="80">
-				</el-table-column>
-				<el-table-column prop="phone" label="联系电话" width="80">
-				</el-table-column>
-				<el-table-column prop="industryName" label="所属行业" width="180">
-				</el-table-column>
-				<el-table-column prop="registeredTime" label="注册时间" width="120">
-				</el-table-column>
-				<el-table-column prop="employeesNum" label="员工人数" width="80">
-				</el-table-column>
-				<el-table-column prop="bachelorAboveNum" label="本科及以上" width="80">
-				</el-table-column>
-				<el-table-column prop="patentNum" label="专利数" width="80">
-				</el-table-column>
-				<el-table-column prop="officeArea" label="办公室面积/m²" width="80">
-				</el-table-column>
-				<!-- <el-table-column prop="taxStatisticsArea" label="税收统计区" width="120">
-				</el-table-column> -->
-				<el-table-column prop="corporateTax" label="企业税收额（万元)" width="140"/>
-				<el-table-column prop="floorNum" label="楼层" width="80"/>
-				<el-table-column prop="officeSpaceType" label="用房类型" width="100"/>
-      </el-table>
-
-    </dialog-drag>
+					<el-table :data="companyDataForms.companiesFullInfo" height="250" border style="width: 100%">
+								<el-table-column prop="buildingName" label="楼宇名称" width="120">
+								</el-table-column>
+								<el-table-column prop="companyName" label="企业名称" width="180">
+								</el-table-column>
+								<el-table-column prop="unifiedSocialCreditCode" label="统一社会信用代码" width="180">
+								</el-table-column>
+								<el-table-column prop="actualOfficeAddress" label="实际办公地址" width="180">
+								</el-table-column>
+								<el-table-column prop="registeredCapital" label="注册资本(万元)" width="120">
+								</el-table-column>
+								<el-table-column prop="legalRepresentative" label="公司法人代表" width="140">
+								</el-table-column>
+								<el-table-column prop="contacts" label="联系人" width="80">
+								</el-table-column>
+								<el-table-column prop="phone" label="联系电话" width="80">
+								</el-table-column>
+								<el-table-column prop="industryName" label="所属行业" width="180">
+								</el-table-column>
+								<el-table-column prop="registeredTime" label="注册时间" width="120">
+								</el-table-column>
+								<el-table-column prop="employeesNum" label="员工人数" width="80">
+								</el-table-column>
+								<el-table-column prop="bachelorAboveNum" label="本科及以上" width="80">
+								</el-table-column>
+								<el-table-column prop="patentNum" label="专利数" width="80">
+								</el-table-column>
+								<el-table-column prop="officeArea" label="办公室面积/m²" width="80">
+								</el-table-column>
+								<el-table-column prop="companyTax[2].revenue" label="2020营收(万元)" width="120">
+								</el-table-column>
+								<el-table-column prop="companyTax[2].tax" label="2020税收(万元)" width="120"/>
+								<el-table-column prop="floorNum" label="楼层" width="80"/>
+								<el-table-column prop="officeSpaceType" label="用房类型" width="100"/>
+					</el-table>
+				</el-tab-pane>
+				<el-tab-pane label="楼宇信息">楼宇信息</el-tab-pane>
+			</el-tabs>
+		</dialog-drag>
 	</div>
 </template>
 
 <script>
 import DialogDrag from 'vue-dialog-drag';
-import { getBuildingFloors,getInfoByBuildingNameAndFloor,getRoomByBuilding} from '@/api/company.js';
+import { getInfoByBuildingNameAndFloor,getRoomByBuilding} from '@/api/company.js';
 
 import filterPanel from './filterComponents/filterPanel.vue'
 
@@ -72,7 +76,7 @@ export default {
 	},
 	data(){
 		return {
-			option: { top: 250, left: 20, height: 350, width: 800, buttonPin: false },
+			option: { top: 250, left: 20, height: 420, width: 800, buttonPin: false },
 			companyDataForms: this.companyDatas,
 		}
 	},
